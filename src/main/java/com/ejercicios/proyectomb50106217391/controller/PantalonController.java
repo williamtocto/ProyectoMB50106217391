c7ebd861f46df6a1ef9726b64fc26e0efe205d4b
Myrian Cutiopala <miriancutiopala123@gmail.com>
2022-07-03 17:16:53 -0500

    Pantalon_service

---
+++ src/main/java/com/ejercicios/proyectomb50106217391/controller/PantalonController.java
@@ -0,0 +1,62 @@
+package com.ejercicios.proyectomb50106217391.controller;
+
+
+import com.ejercicios.proyectomb50106217391.model.Pantalon;
+import com.ejercicios.proyectomb50106217391.model.Zapato;
+import com.ejercicios.proyectomb50106217391.service.PantalonService;
+import com.ejercicios.proyectomb50106217391.service.ZapatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pantalon")
+public class PantalonController {
+
+    @Autowired
+    PantalonService pantalonService;
+
+    @GetMapping("/listar")
+    public List<Pantalon> listar() {
+
+        //Obtener la lista de Pantalon
+        List<Pantalon> lista= pantalonService.findByAll();
+        lista.stream().forEach(c-> c.setCosto_total(c.getCantidad()*c.getCosto()));
+        //Actualizar en la base
+        for (int i=0;i<lista.size();i++){
+            Pantalon pantalon = pantalonService.findById(lista.get(i).getId());
+            pantalon.setCosto_total(pantalon.getCantidad()* pantalon.getCosto());
+            pantalonService.save(pantalon);
+        }
+        return lista;
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Pantalon> crearUsuario(@RequestBody Pantalon p) {
+        return new ResponseEntity<>(pantalonService.create(p), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Pantalon> eliminarPantalon(@PathVariable Integer id) {
+        pantalonService.eliminar(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public Pantalon actualizarPantalon(@PathVariable Integer id, @RequestBody Pantalon p) {
+        Pantalon pantalonActual = pantalonService.findById(id);
+        pantalonActual.setMarca(p.getMarca());
+        pantalonActual.setModelo(p.getModelo());
+        pantalonActual.setFabricante(p.getFabricante());
+        pantalonActual.setCantidad(p.getCantidad());
+        pantalonActual.setCosto(p.getCosto());
+        pantalonActual.setCosto_total(p.getCantidad()*p.getCosto());
+        pantalonActual.setOrigen(p.getOrigen());
+
+        return pantalonService.save(pantalonActual);
+
+    }
+}
---
+++ src/main/java/com/ejercicios/proyectomb50106217391/service/PantalonServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ejercicios.proyectomb50106217391.service;
+
+
+import com.ejercicios.proyectomb50106217391.model.Casa;
+import com.ejercicios.proyectomb50106217391.model.Pantalon;
+import com.ejercicios.proyectomb50106217391.repository.PantalonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PantalonServiceImpl implements PantalonService{
+
+    @Autowired
+    PantalonRepository pantalonRepository;
+
+    @Override
+    public Pantalon create(Pantalon p) {
+        return pantalonRepository.save(p);
+    }
+
+    @Override
+    public Pantalon save(Pantalon pantalon) {
+        return pantalonRepository.save(pantalon);
+    }
+
+    @Override
+    public Pantalon findById(Integer id) {
+        return pantalonRepository.findById(id).orElse(new Pantalon());
+    }
+
+    @Override
+    public List<Pantalon> findByAll() {
+        return pantalonRepository.findAll();
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        pantalonRepository.deleteById(id);
+    }
+}
