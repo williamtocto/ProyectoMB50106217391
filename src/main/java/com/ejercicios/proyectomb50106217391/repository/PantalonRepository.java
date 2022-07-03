686ea8b5ccbb233f52103832fb5eef060d688581
Myrian Cutiopala <miriancutiopala123@gmail.com>
2022-07-03 02:53:27 -0500

    comit 2

---
+++ src/main/java/com/ejercicios/proyectomb50106217391/repository/PantalonRepository.java
@@ -0,0 +1,13 @@
+package com.ejercicios.proyectomb50106217391.repository;
+
+import com.ejercicios.proyectomb50106217391.model.Casa;
+import com.ejercicios.proyectomb50106217391.model.Pantalon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.ArrayList;
+
+public interface PantalonRepository extends JpaRepository<Pantalon, Integer> {
+
+    ArrayList<Pantalon> findByCodigo(String codigo);
+
+}
